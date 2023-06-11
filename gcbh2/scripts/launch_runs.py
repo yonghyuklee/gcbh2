b7da12adeb2825277577193c95c7a6fad6156149
Santiago Vargas <santiagovargas921@gmail.com>
2023-06-11 11:34:12 -0700

    folder err

--- gcbh2/scripts/launch_runs.py
+++ gcbh2/scripts/launch_runs.py
@@ -5,7 +5,7 @@ def main():
     # go through subfolders of depth 0 and if the file job_gpu.sh exists, run it
     for folder in os.listdir("."):
         if os.path.isdir(folder):
-            if "job_gpu.sh" in os.listdir("{}/{}".format(folder)):
+            if "job_gpu.sh" in os.listdir("./{}".format(folder)):
                 os.chdir("./{}".format(folder))
                 os.system("sbatch job_gpu.sh")
                 os.chdir("../")
